197011cb4fb04dcfc1cccc71b54c9107b7823329
Ankoven <3804109@qq.com>
2020-11-23 11:34:53 +0800

    交作业

---
+++ 刘煜堃-3018216233/CarControl/Assets/Scripts/CarCtrl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCtrl : MonoBehaviour
+{
+    float MoveSpeed = 5;//前进速度
+    float RotateSpeed = 65;//转向速度
+
+    void Update()
+    {
+
+        if (this.transform.up.y > 0 && this.transform.up.y <= 10)
+            if (Input.GetKey(KeyCode.W))//当按下w键时
+            {
+                if (MoveSpeed <= 40)//当速度不大于60时，按下w键可以使车辆提速
+                {
+                    MoveSpeed = MoveSpeed + 10 * Time.deltaTime;
+                }
+
+                this.transform.Translate(Vector3.forward * Time.deltaTime * MoveSpeed);
+
+                if (Input.GetKey(KeyCode.A))//在按下w键的同时按下a键，车辆向左前方前进
+                {
+                    this.transform.Rotate(Vector3.up * Time.deltaTime * -RotateSpeed);//
+                }
+
+                else if (Input.GetKey(KeyCode.D))//在按下w键的同时按下d键，车辆向右前方前进
+                {
+                    this.transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed);
+                }
+            }
+
+            else if (Input.GetKey(KeyCode.S))//当按下s键时
+            {
+                MoveSpeed = 15;
+
+                this.transform.Translate(Vector3.forward * Time.deltaTime * -MoveSpeed);
+                if (Input.GetKey(KeyCode.A))//在按下s键的同时按下a键，车辆向左后方倒车
+                {
+                    this.transform.Rotate(Vector3.up * Time.deltaTime * -RotateSpeed);
+                }
+
+                else if (Input.GetKey(KeyCode.D))//在按下s键的同时按下d键，车辆向右后方倒车
+                {
+                    this.transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed);
+                }
+            }
+            //如果车辆并非在行驶过程中，那么只按下a键或d键不会有任何效果
+    }
+}
---
+++ 刘煜堃-3018216233/CarControl/Assets/Scripts/Follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    private Transform target;
+    private Vector3 fixedDistance;
+    private Vector3 tempPos;
+
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;//找到标签为player的目标物体
+        fixedDistance = new Vector3(0, 3.35f, -7.33f);//相机处于赛车背后的初始位置，需要进行测试
+    }
+
+    void FixedUpdate()
+    {
+        tempPos = target.TransformDirection(fixedDistance) + target.position;
+        transform.position = Vector3.Lerp(transform.position, tempPos, Time.fixedDeltaTime * 3);
+        transform.LookAt(target);//使相机自身z坐标对齐物体的自身z坐标（注意不是世界坐标）
+    }
+}
